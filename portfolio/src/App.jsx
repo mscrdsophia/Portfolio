2df79c0dfc163ef5d9f9bbbf714d5487ae8324a0
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2025-06-21 11:52:29 +0200

    hero section

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+
+
+function App() {
+  
+
+  return (
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      
+     
+      </Routes>
+    </Router>
+  </div>
+  )
+}
+
+export default App
---
+++ portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen p-6 md:p-8 bg-white">
+      <div className="max-w-6xl mx-auto">
+
+        {/* Header Section */}
+        <div className="flex  md:flex-row justify-between items-start md:items-start mb-12 gap-6 md:gap-0">
+          {/* Name and Email */}
+          <div className="flex flex-col items-center items-start md:items-start text-center md:text-left">
+            <h1 className="text-5xl md:text-7xl font-black text-black leading-none mb-2">
+              SOPHIA
+            </h1>
+            <h1 className="text-5xl md:text-7xl font-black text-black items-start  leading-none">
+              MASCARDO
+            </h1>
+            <h2 className="text-base md:text-lg text-gray-600 mt-4 items-start  font-normal">
+              sophia.web.dev24@gmail.com
+            </h2>
+          </div>
+
+          {/* Actual Picture */}
+          <div className="self-center md:self-start">
+            <div className="w-20 h-20 md:w-50 md:h-50 rounded-full overflow-hidden border-2 shadow-md">
+              <img
+                src="/sophia.JPG"
+                alt="Sophia Mascarado"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Bio Section */}
+        <div className="flex justify-center md:justify-end">
+          <div className="max-w-2xl text-center md:text-right">
+            <p className="text-base md:text-xl text-gray-800 leading-relaxed">
+              Hi, I’m a web developer with a background in retail leadership, and passionate about building user-friendly digital solutions.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
